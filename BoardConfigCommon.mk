CyanogenMod/android_device_lge_g3-common

g3-common: Fix radio data
 * Remove eri.xml overlay
 * Add telephony overlay
 * Custom RIL to handle GET_SIM_STATUS response

BoardConfigCommon.mk
@@ -122,5 +122,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE -DLPA_DEFAULT_BUFFER_SIZE=512
 
+BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
+
 # inherit from the proprietary version
 -include vendor/lge/g3-common/BoardConfigVendor.mk

bangprovn/android_device_lge_v4xx-common

Conflicts:
	BoardConfigCommon.mk
	system_prop.mk
Change-Id: I88b2e28720122cd0e1c95b700961c3c1b2b98fb0

BoardConfigCommon.mk
@@ -121,6 +121,9 @@ TARGET_RECOVERY_FSTAB := device/lge/v4xx-common/rootdir/etc/fstab.e7
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# RIL
+BOARD_RIL_CLASS := ../../../device/lge/v4xx-common/ril/
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 # TODO